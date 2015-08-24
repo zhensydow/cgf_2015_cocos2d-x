37a6a51a6dccfec0a37c867fee11104f296122ca
Luis Cabellos <zhen.sydow@gmail.com>
2015-08-24 17:56:54 +0200

    add background

--- Classes/SceneMainMenu.cpp
+++ Classes/SceneMainMenu.cpp
@@ -27,6 +27,7 @@ bool SceneMainMenu::init(){
     auto scr_size = Director::getInstance()->getVisibleSize();
     auto scr_origin = Director::getInstance()->getVisibleOrigin();
 
+    // create a debug name
     auto label = Label::createWithTTF( "Main Menu", "fonts/gentium.ttf", 24 );
     auto label_size = label->getContentSize();
 
@@ -36,6 +37,14 @@ bool SceneMainMenu::init(){
                 - label_size.height/2.0f - 10.0f} );
 
     this->addChild( label, 3 );
+
+    // create a background
+    auto bg = Sprite::create( "bg_menu.png" );
+
+    bg->setPosition( { scr_origin.x + scr_size.width/2.0f,
+                scr_origin.y + scr_size.height/2.0f } );
+
+    this->addChild( bg, -1 );
 
     return true;
 }
